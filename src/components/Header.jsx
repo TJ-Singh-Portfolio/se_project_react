81df8fac0bcceb5b2700f81eb88d39c4dd4ea2cd
TJ Singh <tsinghbusiness@hotmail.com>
2024-06-12 16:32:42 -0700

    Initial commit. Initialize Vite. Add basic structure of Header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import "../blocks/Header.css";
+import headerAvatar from "../assets/avatar.png";
+function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <div className="header">
+      <div className="header__container-left">
+        <img className="header__logo" src="" alt="What to Wear Logo" />
+        <p className="header__date-city">{currentDate}</p>
+      </div>
+      <div className="header__container-right">
+        <button className="header__button" type="button">
+          Add Clothes
+        </button>
+        <p className="header__name">Terrence Tegegne</p>
+        <img
+          className="header__avatar"
+          src={headerAvatar}
+          alt="Avatar Picture"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
