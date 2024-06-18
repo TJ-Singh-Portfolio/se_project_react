506bdd76d56d5c55f613cf891ded6c33916a6e0d
TJ Singh <tsinghbusiness@hotmail.com>
2024-06-18 16:29:37 -0700

    Add styles to ModalWithForm.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,38 @@ import viteLogo from "/vite.svg";
 //import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import ModalWithForm from "./ModalWithForm";
 import getInfo from "../utils/weatherApi";
 
 function App(props) {
   let apiInfo = [];
   getInfo().then((res) => {
     apiInfo = [...res];
+    console.log(apiInfo);
   });
   //const [temp, city] = apiInfo;
-  console.log(apiInfo);
   //All of the problems you are running into are coming up due to ansynchroneity.
+  function handleClick(event) {
+    this.addEventListener("mousedown", (event) => {
+      if (
+        event.target.classList.contains("modal_opened") ||
+        event.target.classList.contains("modal__close")
+      ) {
+        this.classList.remove("modal_opened");
+      }
+    });
+  }
+  //Add functionality for "Add Clothes" button before adding the close funcionality.
 
   return (
     <>
-      <Header name="Test" />
+      <Header name={apiInfo[0]} />
+      <ModalWithForm
+        name="form1"
+        title="New Garment"
+        buttonText="Add Garment"
+        onClick={handleClick}
+      />
       <Footer />
     </>
   );
--- src/components/ModalWithForm.jsx
+++ src/components/ModalWithForm.jsx
@@ -1,14 +1,24 @@
+import "../blocks/ModalWithForm.css";
+
 function ModalWithForm(props) {
-  <div className="modal">
-    <div className={`modal__container modal_type_${name}`}>
-      <h2 className="modal__title">{props.title}</h2>
-      <button className="modal__close" type="button"></button>
-      <form action="" className="modal__form" name={props.name}>
-        {props.children}
-      </form>
-      <button className="modal__submit" type="submit">
-        {props.buttonText}
-      </button>
+  return (
+    <div className="modal">
+      <div className={`modal__container modal_type_${name}`}>
+        <h2 className="modal__title">{props.title}</h2>
+        <button
+          className="modal__close"
+          type="button"
+          onClick={props.onClose}
+        ></button>
+        <form action="" className="modal__form" name={props.name}>
+          {props.children}
+        </form>
+        <button className="modal__submit" type="submit">
+          {props.buttonText}
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 }
+
+export default ModalWithForm;
